44cfb977116869133fde09a8d3ff9b7c0f44f29c
shota <syo06ta18@gmail.com>
2017-06-17 00:27:19 +0900

    change all

--- rnn_gan_main.py
+++ rnn_gan_main.py
@@ -2,9 +2,12 @@
 #signal generate
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, merge, Input
+from keras.models import Model
 from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import Bidirectional
+from keras.regularizers import l2
+# from keras.utils.vis_utils import plot_model
 import tensorflow as tf
 import numpy as np
 import os, sys
@@ -18,25 +21,57 @@ def create_random_input():
 
 
 def form_discriminator():
-    discriminator=Sequential([Bidirectional(LSTM(input_dim=1,output_dim=hidden_cell,forget_bias_init=1.0,W_regularizer=l2(0.01))),Dence(output_dim=1,activation='sigmoid')])
+    discriminator=Sequential()
+    # discriminator.add(Bidirectional(LSTM(units=(lstm_cell,),unit_forget_bias=True,recurrent_regularizer=l2(0.01)),input_shape=(1,1)))
+    discriminator.add(LSTM(input_shape=(None,1,),units=lstm_cell,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=False))
+    # discriminator.add(Dense(input_shape=(1,),units=1,activation='sigmoid'))
+    discriminator.add(Dense(units=1,activation='sigmoid'))
 
     return discriminator
 
 
 def form_generator():
-    generator=Sequential([LSTM(input_dim=1,output_dim=hidden_cell,forget_bias_init=1.0,W_regularizer=l2(0.01)),Dence(output_dim=1,activation='sigmoid')])
+    generator=Sequential()
+    generator.add(Bidirectional(LSTM(units=lstm_cell,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=False),input_shape=(None,1)))
+    # generator.add(Dense(input_shape=(1,),units=1,activation='sigmoid'))
+    generator.add(Dense(units=1))
+    generator.add(Activation('linear'))
 
     return generator
 
 
-def form_gan():
+# def form_gan():
 
 
 if __name__=='__main__':
-    D=form_discriminator
-    G=form_generator
+    # Input=Input(shape=(1,))
+    # model1=Dense(units=10)(Input)
+    # model2=Dense(units=30)(Input)
+    # a=[]
+    # a.append(model1)
+    # a.append(model2)
+    # merged_m=merge(a,mode='sum')
+    # model=Model(inputs=Input,outputs=merged_m)
+    # model.compile(optimizer='sgd',loss='binary_crossentropy')
+    # model.summary()
+    # sys.exit()
+
+    D=form_discriminator()
+    G=form_generator()
     D.compile(optimizer='sgd',loss='binary_crossentropy')
-    set_trainable(D,False)
+    D.trainable=False
+    print('formed D-------\n')
+    G.compile(optimizer='sgd',loss='binary_crossentropy')
+    G.trainable=False
+    print('formed G-------\n')
+    print('model G')
+    G.summary()
+    print('\nmodel D')
+    D.summary()
     GAN=Sequential([G,D])
+    print('form GAN\n')
     GAN.compile(optimizer='sgd',loss='binary_crossentropy')
-    print(D.layers[0])
+    GAN.summary()
+    # D.summary()
+    # plot_model(D,to_file='model.png')
+    print('end')
