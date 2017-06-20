f17d1ceeec7ca157c78c3b5d7be0305c64eb01c4
shota-h <syo06ta18@gmail.com>
2017-06-20 23:35:41 +0900

    change layer

--- test_batch_train.py
+++ test_batch_train.py
@@ -2,7 +2,7 @@
 #signal generate
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, merge, Input
+from keras.layers import Dense, Activation, merge, Input, pooling
 from keras.models import Model
 from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import Bidirectional
@@ -12,24 +12,34 @@ import tensorflow as tf
 import numpy as np
 from keras import backend as K
 import matplotlib.pyplot as plt
-import os, sys, json
+import os, sys, json, datetime
 
 
 file_dir=os.path.abspath(os.path.dirname(__file__))
 lstm_cell=350
-
+today=datetime.date.today()
 
 
 def create_random_input(signal_num):
     return np.random.uniform(low=-1,high=1,size=[signal_num,signal_len,1])
 
 
+def d_object(y_true,y_pred):
+    return 1/signal_len*K.sum(-1*K.log(y_pred)-K.log(1-y_pred))
+
+
+def g_object(y_true,y_pred):
+    return
+
+
 def form_discriminator():
+    # TODO denseを共有レイヤーに
     discriminator=Sequential()
     # discriminator.add(Bidirectional(LSTM(units=(lstm_cell,),unit_forget_bias=True,recurrent_regularizer=l2(0.01)),input_shape=(1,1)))
-    discriminator.add(Bidirectional(LSTM(units=lstm_cell,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=False),merge_mode='concat',input_shape=(signal_len,1)))
+    discriminator.add(Bidirectional(LSTM(units=lstm_cell,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=True),merge_mode='concat',input_shape=(signal_len,1)))
     # discriminator.add(Dense(input_shape=(1,),units=1,activation='sigmoid'))
     discriminator.add(Dense(units=1,activation='sigmoid'))
+    # discriminator.add(pooling.AveragePooling1D(pool_length=signal_len,strides=None))
 
     return discriminator
 
@@ -49,12 +59,14 @@ def form_generator():
 
 if __name__=='__main__':
     train_x=np.load(file_dir+'/ecg_small.npy')
-    train_y=np.zeros([train_x.shape[0]])
-
     global signal_len
     # signal_len=train_x.shape[1]
     signal_len=train_x.shape[1]
-    print(signal_len)
+
+    train_y=np.zeros([train_x.shape[0],signal_len])
+
+
+    print('signal_length',signal_len)
     print('\n----setup----\n')
     D=form_discriminator()
     print('\n----form D----\n')
@@ -80,29 +92,31 @@ if __name__=='__main__':
 
     print('\n----train step----\n')
     get_hidden_layer=K.function([GAN.layers[0].input],[GAN.layers[0].output])
+    # G.summary()
+    # sys.exit()
     mat_d=[]
     mat_g=[]
-    for epoch in range(500):
+    # sys.exit()
+    for epoch in range(50):
         print('epoch:{0}'.format(epoch+1))
         np.random.shuffle(train_x)
         signal_num=42
         test_x=create_random_input(signal_num)
-        # print(test_x.shape)
-        # sys.exit()
-        test_y=np.zeros([int(test_x.shape[0])])
+        test_y=np.zeros([test_x.shape[0],signal_len])
         hidden_output=get_hidden_layer([test_x])
         hidden_output=np.array(hidden_output)
         hidden_output=hidden_output[0,:,:,:]
         d_x=np.append(train_x,hidden_output,axis=0)
-        d_y=np.append(train_y,np.ones([int(hidden_output.shape[0])]))
-        history_d=D.fit([d_x],[d_y],epochs=1,batch_size=int(d_x.shape[0]/2),verbose=0)
-        history_g=GAN.fit([test_x],[test_y],epochs=1,batch_size=5,verbose=0)
-        if epoch%10==0:
-            # print(type(history_d.history['loss']))
-            # print('\n----loss D----\n',history_d.history['loss'])
+        d_y=np.append(train_y,np.ones([hidden_output.shape[0],signal_len]),axis=0)
+        print(d_y.shape)
+        history_d=D.fit([d_x],[d_y.reshape(d_y.shape[0],1000,1)],epochs=1,batch_size=int(d_x.shape[0]/6),verbose=0)
+        # print('--------\n',test_x.shape,test_y.shape)
+        history_g=GAN.fit([test_x],[test_y.reshape(test_y.shape[0],1000,1)],epochs=1,batch_size=int(test_x.shape[0]/6),verbose=0)
+        if (epoch+1)%10==0:
             mat_d.append(history_d.history['loss'])
-            # print('\n----loss G----\n',history_g.history['loss'])
             mat_g.append(history_g.history['loss'])
+            print('\n----loss d----\n',history_d.history['loss'])
+            print('\n----loss g----\n',history_g.history['loss'])
         # met_curve=np.append(met_curve,[history_d['loss'][-1],history_g['loss'][-1]],axis=0)
 
     mat_d=np.array(mat_d)
@@ -112,7 +126,7 @@ if __name__=='__main__':
     print('\n----trained D----\n')
     print('\n----trained G----\n')
     model_json=GAN.to_json()
-    f=open('model_gan.json','w')
+    f=open('model_gan_.json','w')
     json.dump(model_json,f)
     GAN.save_weights('gan_param.hdf5')
     model_json=D.to_json()
