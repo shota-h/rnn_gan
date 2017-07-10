e3c5e0387acb8a8096b36f90b3e70b40faba7b02
shota-h <syo06ta18@gmail.com>
2017-07-10 16:08:29 +0900

    change model function

--- rnn_gan_main.py
+++ rnn_gan_main.py
@@ -1,108 +1,142 @@
 #rnn GAN
-#signal generate
-# TODO パラメータの調整必要
+#-----signal generate-----
+
 from keras.models import Sequential
-from keras.layers import Dense, Activation, merge, Input, pooling
+from keras.layers import Dense, Activation,  pooling
 from keras.models import Model
 from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import Bidirectional
 from keras.regularizers import l2
-# from keras.utils.vis_utils import plot_model
 import tensorflow as tf
 import numpy as np
 from keras import backend as K
 import matplotlib.pyplot as plt
-import os, sys, json
+import os, sys, json, datetime, itertools
 
+config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
+session = tf.Session(config=config)
+K.set_session(session)
 
-file_dir=os.path.abspath(os.path.dirname(__file__))
-lstm_cell=350
-
+args=sys.argv
+cell_num = int(args[1])
+layer_num = int(args[2])
+epoch_num = int(args[3])
+file_dir = os.path.abspath(os.path.dirname(__file__))
+os.mkdir('{0}/rnn_gan_parallel_train/layer{1}_cell{2}'.format(file_dir, layer_num, cell_num))
+file_path = '{0}/rnn_gan_parallel_train/layer{1}_cell{2}'.format(file_dir, layer_num, cell_num)
 
 
 def create_random_input(signal_num):
-    return np.random.uniform(low=-1,high=1,size=[signal_num,signal_len,1])
+    return np.random.uniform(low=0,high=1,size=[signal_num,signal_len,1])
 
 
 def form_discriminator():
+    # TODO denseを共有レイヤーに
     discriminator=Sequential()
-    # discriminator.add(Bidirectional(LSTM(units=(lstm_cell,),unit_forget_bias=True,recurrent_regularizer=l2(0.01)),input_shape=(1,1)))
-    discriminator.add(LSTM(input_shape=(signal_len,1),units=lstm_cell,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=True))
-    # discriminator.add(Dense(input_shape=(1,),units=1,activation='sigmoid'))
+    discriminator.add(Bidirectional(LSTM(units=cell_num,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=True),input_shape=(signal_len,1)))
+    for i in range(layer_num-1):
+        discriminator.add(Bidirectional(LSTM(units=cell_num,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=True)))
     discriminator.add(Dense(units=1,activation='sigmoid'))
-    discriminator.add(pooling.AveragePooling1D(pool_length=1))
-
+    discriminator.add(pooling.AveragePooling1D(pool_size=signal_len,strides=None))
     return discriminator
 
 
 def form_generator():
     generator=Sequential()
-    generator.add(Bidirectional(LSTM(units=lstm_cell,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=True),input_shape=(signal_len,1),merge_mode='sum'))
-    # generator.add(Dense(input_shape=(1,),units=1,activation='sigmoid'))
-    generator.add(Dense(units=1))
-    generator.add(Activation('linear'))
+    generator.add(LSTM(input_shape=(signal_len,1),units=cell_num,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=True))
+    for i in range(layer_num-1):
+        generator.add(LSTM(input_shape=(signal_len,1),units=cell_num,unit_forget_bias=True,recurrent_regularizer=l2(0.01),return_sequences=True))
+    generator.add(Dense(units=1,activation='sigmoid'))
 
     return generator
-
 
 # def form_gan():
 
 
 if __name__=='__main__':
-    train_x=np.load(file_dir+'/ecg_small.npy')
-    train_y=np.ones([train_x.shape[0]])
-
+    train_x=np.load(file_dir+'/dataset/ecg_only_mini.npy')
     global signal_len
-    # signal_len=train_x.shape[1]
     signal_len=train_x.shape[1]
-    print('\n----setup----\n')
-    D=form_discriminator()
-    print('\n----form D----\n')
-    G=form_generator()
-    print('\n----form G----\n')
-    D.compile(optimizer='sgd',loss='binary_crossentropy')
-    print('\n----compile D----\n')
-    print('\n----model D----\n')
-    D.summary()
-    D.trainable=False
-    # G.compile(optimizer='sgd',loss='binary_crossentropy')
-    # G.trainable=False
-    # print('formed G-------\n')
-    # print('model G')
-    # G.summary()
+    varidation_x=train_x
+    # varidation_x=varidation_x.reshape(1,signal_len,1)
 
+    signal_num=int(train_x.shape[0])
+
+    print('signal_length',signal_len)
+    print('\n----setup----\n')
+    G=form_generator()
+    G.summary()
+    # sys.exit()
+    D=form_discriminator()
+    # G=form_generator()
+    D.compile(optimizer='Adam',loss='binary_crossentropy')
+    D.trainable=False
+    # G.compile(optimizer='Adam',loss='mean_squared_error')
     GAN=Sequential([G,D])
-    print('form GAN\n')
-    GAN.compile(optimizer='sgd',loss='binary_crossentropy')
-    # GAN.trainable=False
-    print('\n----model GAN----\n')
-    GAN.summary()
+    GAN.compile(optimizer='Adam',loss='binary_crossentropy')
 
     print('\n----train step----\n')
+    hidden_y=np.ones([signal_num,1,1])
+    train_y=np.zeros([signal_num,1,1])
+    random_y=np.zeros([signal_num*2,1,1])
+    varidation_y=np.zeros([1,1,1])
+    varidation_y=np.append(varidation_y,np.ones([1,1,1]),axis=0)
+    mat_d=[]
+    mat_g=[]
+    mat_pre_d=[]
+    mat_pre_g=[]
+    # save model
+    model_json=GAN.to_json()
+    f=open(file_dir+'/rnn_gan_main/model_gan_layer3_cell300.json','w')
+    json.dump(model_json,f)
+    model_json=D.to_json()
+    f=open(file_dir+'/rnn_gan_main/model_dis_layer3_cell300.json','w')
+    json.dump(model_json,f)
 
-    # for epoch in range(1,100):
-    #     print('epoch:{0}'.format(epoch))
+    for epoch in range(100000):
+        np.random.shuffle(train_x)
+        random_x=create_random_input(signal_num)
+        hidden_output=G.predict([random_x])
+        history_d=D.train_on_batch([train_x],[train_y],sample_weight=None)
+        history_d=D.train_on_batch([hidden_output],[hidden_y],sample_weight=None)
+        history_gan=GAN.train_on_batch([random_x],[np.zeros([1,1,1])],sample_weight=None)
+        # history_g=G.train_on_batch([random_x],[train_x],sample_weight=None)
+        if (epoch+1)%1000==0:
+            print('epoch:{0}'.format(epoch+1))
+            hidden_output=G.predict([create_random_input(1)])
+            plt.plot(train_x[0,:,0])
+            plt.plot(hidden_output[0,:,0],'.-')
+            plt.savefig(file_dir+'/rnn_gan_main/epoch{0}_generated.png'.format(epoch+1))
+            plt.clf()
+            varidation_x_d=np.append(varidation_x,hidden_output,axis=0)
+            loss_d=D.test_on_batch([varidation_x_d],[varidation_y])
+            random_x=create_random_input(1)
+            loss_g=GAN.test_on_batch([random_x],[np.zeros([1,1,1])])
+            print('\n----loss d----\n',loss_d)
+            print('\n----loss g----\n',loss_g)
+            predict_d=D.predict([varidation_x])
+            predict_g=D.predict([hidden_output])
+            print('\n----predict train signal----\n',predict_d[0][0][0])
+            print('\n----predict generate signal----\n',predict_g[0][0][0])
+            mat_d.append(loss_d)
+            mat_g.append(loss_g)
+            mat_pre_d.append(predict_d[0][0][0])
+            mat_pre_g.append(predict_g[0][0][0])
+            # sys.exit()
 
-
-    d_loss=D.fit(train_x,train_y,epochs=100,batch_size=21,verbose=1)
+    mat_d=np.array(mat_d)
+    mat_g=np.array(mat_g)
+    np.save(file_dir+'/rnn_gan_mian/loss_d.npy',mat_d)
+    np.save(file_dir+'/rnn_gan_main/loss_g.npy',mat_g)
+    mat_d=np.array(mat_pre_d)
+    mat_g=np.array(mat_pre_g)
+    np.save(file_dir+'/rnn_gan_main/predict_d.npy',mat_pre_d)
+    np.save(file_dir+'/rnn_gan_main/predict_g.npy',mat_pre_g)
     print('\n----trained D----\n')
-    print('loss D',d_loss.history['loss'][-1])
-    signal_num=10
-    test_x=create_random_input(signal_num)
-    test_y=np.ones([int(test_x.shape[0])])
-    # print(test_x.shape)
-    # print(test_y.shape)
-    gan_loss=GAN.fit(test_x,test_y,epochs=10,batch_size=5,verbose=0)
-    print('\n----trained GAN----\n')
-    print('loss GAN',gan_loss.history['loss'][-1])
-    print(GAN.layers)
-    print(D.predict(create_random_input(1)))
-    # model_json=GAN.to_json()
-    # f=open('model_gan.json','w')
-    # json.dump(model_json,f)
-    # sys.exit()
-    # get_hidden_layer_output=K.function([GAN.layers[2]])
+    print('\n----trained G----\n')
 
-    # gan_acc=GAN.predict(create_random_input(10))
-    # print('gan predict',gan_acc)
+
+    GAN.save_weights(file_dir+'/rnn_gan_main/gan_param_layer3_cell300_epoch{0}.hdf5'.format(epoch+1))
+
+    D.save_weights(file_dir+'/rnn_gan_main/dis_param_layer3_cell300_epoch{0}.hdf5'.format(epoch+1))
     K.clear_session()
