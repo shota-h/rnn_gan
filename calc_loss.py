9618b4764d564fc84c979b2df0669a0dcc5e6d64
shota <shota-h>
2018-07-04 21:39:41 +0900

    correspond k-nn

--- calc_loss.py
+++ calc_loss.py
@@ -5,16 +5,17 @@ import os, itertools, sys
 file_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-def dtw(vec1, vec2):
+def dtw(vec1, vec2, band_flag=False, num_band=0):
     loss_vec = []
     for k1, k2 in itertools.product(range(vec1.shape[0]),range(vec2.shape[0])):
         d = np.zeros([vec1.shape[1]+1, vec2.shape[1]+1])
         d[:] = np.inf
         d[0, 0] = 0
-        for i in range(1, d.shape[0]):
-            for j in range(1, d.shape[1]):
-                cost = abs(vec1[k1, i-1]-vec2[k2, j-1])
-                d[i, j] = cost + min(d[i-1, j], d[i, j-1], d[i-1, j-1])
+        if band_flag is False: num_band = d.shape[0]
+        for i, j in intertools.product(range(1, d.shape[0]), range(1, d.shape[1])):
+            if num_band <= abs(i - j): continue
+            cost = abs(vec1[k1, i-1]-vec2[k2, j-1])
+            d[i, j] = cost + min(d[i-1, j], d[i, j-1], d[i-1, j-1])
         loss_vec.append(d[-1,-1])
     loss_vec = np.array(loss_vec)
     mean_loss = np.mean(loss_vec)
