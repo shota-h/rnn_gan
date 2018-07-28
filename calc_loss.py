18ec183706809272f16b539f1cee6efe7b1bf601
shota <shota-h>
2018-07-28 13:57:41 +0900

    cosmetic change

--- calc_loss.py
+++ calc_loss.py
@@ -6,34 +6,34 @@ file_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 def dtw(vec1, vec2, band_flag=False, num_band=0):
-    loss_vec = []
+    dst = []
     for k1, k2 in itertools.product(range(vec1.shape[0]),range(vec2.shape[0])):
         d = np.zeros([vec1.shape[1]+1, vec2.shape[1]+1])
         d[:] = np.inf
         d[0, 0] = 0
         if band_flag is False: num_band = d.shape[0]
-        for i, j in intertools.product(range(1, d.shape[0]), range(1, d.shape[1])):
+        for i, j in itertools.product(range(1, d.shape[0]), range(1, d.shape[1])):
             if num_band <= abs(i - j): continue
             cost = abs(vec1[k1, i-1]-vec2[k2, j-1])
             d[i, j] = cost + min(d[i-1, j], d[i, j-1], d[i-1, j-1])
-        loss_vec.append(d[-1,-1])
-    loss_vec = np.array(loss_vec)
-    mean_loss = np.mean(loss_vec)
-    std_loss = np.std(loss_vec)
+        dst.append(d[-1,-1])
+    dst = np.array(dst).reshape(vec1.shape[0], vec2.shape[0])
+    mean_dst = np.mean(dst, axis=1)
+    std_dst = np.std(dst, axis=1)
 
-    return loss_vec, mean_loss, std_loss
+    return dst, mean_dst, std_dst
 
 
 def mse(vec1, vec2):
     dst = []
     for k1, k2 in itertools.product(range(vec1.shape[0]),range(vec2.shape[0])):
         diff = (vec1[k1] - vec2[k2])**2
-        loss = np.mean(diff)
-        dst.append(loss)
+        diff = np.mean(diff)
+        dst.append(diff)
 
-    dst = np.array(dst)
-    mean_dst = np.mean(dst)
-    std_dst = np.std(dst)
+    dst = np.array(dst).reshape(vec1.shape[0], vec2.shape[0])
+    mean_dst = np.mean(dst, axis=1)
+    std_dst = np.std(dst, axis=1)
 
     return dst, mean_dst, std_dst
 
