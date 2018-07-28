f64c4c10dcf765ab6ce0cb0810ed1d45c34737c6
shota <shota-h>
2018-07-28 13:58:29 +0900

    cosmetic change

--- similar_verifit.py
+++ similar_verifit.py
@@ -4,81 +4,61 @@ import matplotlib.pyplot as plt
 import os
 import csv
 import argparse
+import calc_loss
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dir', type=str, default='rnn_gan', help='select dir')
+parser.add_argument('--datadir', type=str, default=None, help='select dir')
 parser.add_argument('--typeflag', type=str, default='normal', help='normal or abnormal')
-parser.add_argument('--dataflag', type=str, default='raw', help='model or raw or gan')
+parser.add_argument('--Aug', type=str, default='train', help='for example train, gan...')
+
 args = parser.parse_args()
-dirs = args.dir
-TYPEFLAG = args.typeflag
-DATAFLAG = args.dataflag
+datadir = args.datadir
+TYPE = args.typeflag
+AUG = args.aug
 
 
 filedir = os.path.abspath(os.path.dirname(__file__))
-filepath = '{0}/similar-verifit/{1}/{2}-{3}'.format(filedir, dirs, DATAFLAG, TYPEFLAG)
+filepath = '{0}/similar-verifit/{1}'.format(filedir, datadir)
 if os.path.exists(filepath) is False:
     os.makedirs(filepath)
 
 
-def dtw(x1, x2):
-    d = np.zeros([x1.shape[0]+1, x2.shape[0]+1])
-    d[:] = np.inf
-    d[0, 0] = 0
-    for i, j in itertools.product(range(1, d.shape[0]), range(1, d.shape[1])):
-        cost = abs(x1[i-1]-x2[j-1])
-        d[i, j] = cost + min(d[i-1, j], d[i, j-1], d[i-1, j-1])
-    return d[-1, -1]
+def evaluate_similality(src1, src2, mode=None):
+    Error = np.zeros([src1.shape[0], src2.shape[0]])
+    for i, j in itertools.product(range(src1.shape[0]), range(src2.shape[0])):
+        if mode == 'mse':
+            Error[i, j] = calc_loss.mse(src1[i], src2[j])
+        elif mode = 'dtw':
+            Error[i, j] = calc_loss.dtw(src1[i], src2[j])
 
-
-def mse(x1, x2):
-    return np.sum(np.abs(x1 - x2))
-
-
-def similar_verifit(loadpath, flag):
-    ecg = np.load(loadpath)
-    ecg = ecg[:50]
-    Error = np.zeros([ecg.shape[0], ecg.shape[0]])
-    for i, j in itertools.product(range(ecg.shape[0]), range(ecg.shape[0])):
-        Error[i, j] = mse(ecg[i], ecg[j])
-    np.save('{0}/{1}-mse.npy'.format(filepath, flag), Error)
     error_mean = [np.mean(Error[i]) for i in range(Error.shape[0])]
     error_std = [np.std(Error[i]) for i in range(Error.shape[0])]
-    E = ['mse', np.mean(error_mean), np.std(error_mean)]
-    with open('{0}/{1}-mean-std.csv'.format(filepath, flag), 'w') as f:
+    
+    E = ['{}'.format(mode), np.mean(error_mean), np.std(error_mean)]
+    with open('{0}/{1}_mean_std.csv'.format(filepath, flag), 'a') as f:
         writer = csv.writer(f, lineterminator='\n')
         writer.writerow(E)
-    np.save('{0}/{1}-mse_mean.npy'.format(filepath, flag), error_mean)
-    np.save('{0}/{1}-mse_std.npy'.format(filepath, flag), error_std)
-    plt.figure(figsize = (16, 9))
-    plt.bar(range(len(error_mean)), error_mean, yerr = error_std)
-    plt.title('mse')
-    plt.savefig('{0}/{1}-mse.png'.format(filepath,flag))
-    plt.close()
-    Error = np.zeros([ecg.shape[0], ecg.shape[0]])
-    for i, j in itertools.product(range(ecg.shape[0]), range(ecg.shape[0])):
-        Error[i, j] = dtw(ecg[i], ecg[j])
-    np.save('{0}/{1}-dtw.npy'.format(filepath, flag), Error)
-    error_mean = [np.mean(Error[i]) for i in range(Error.shape[0])]
-    error_std = [np.std(Error[i]) for i in range(Error.shape[0])]
-    E = ['dtw', np.mean(error_mean), np.std(error_mean)]
-    with open('{0}/{1}-mean-std.csv'.format(filepath, flag), 'a') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow(E)
-    plt.figure(figsize = (16, 9))
-    plt.bar(range(len(error_mean)), error_mean, yerr = error_std)
-    plt.title('dtw')
-    plt.savefig('{0}/{1}-dtw.png'.format(filepath, flag))
-    plt.close()
 
+    np.save('{0}/{1}-{2}.npy'.format(filepath, flag, mode), Error)
+    np.save('{0}/{1}-{2}_mean.npy'.format(filepath, flag, mode), error_mean)
+    np.save('{0}/{1}-{2}_std.npy'.format(filepath, flag, mode), error_std)
+    plt.figure(figsize = (16, 9))
+    plt.bar(range(len(error_mean)), error_mean, yerr = error_std)
+    plt.title('{}'.format(mode))
+    plt.savefig('{0}/{1}-{2}.png'.format(filepath,flag, mode))
+    plt.close()
+    
+
+def similar_verifit(loadpath):
+    src = np.load(loadpath)
+    src = ecg[:50]
+    evaluate_similality(src, mode='mse')
+    evaluate_similality(src, mode='dtw')
+    
 
 def main():
-    # ecg = np.load('{0}/dataset/normal_normalized.npy'.format(filedir))
-    # loadpath = '{0}/dataset/ecg_abnormal_aug.npy'.format(filedir)
-    loadpath = '{0}/dataset/{1}_{2}.npy'.format(filedir, TYPEFLAG, DATAFLAG)
-    similar_verifit(loadpath, DATAFLAG)
-    loadpath = '{0}/dataset/{1}_gan_based_{2}.npy'.format(filedir, TYPEFLAG, DATAFLAG)
-    similar_verifit(loadpath, 'GAN')
+    loadpath = '{0}/dataset/{1}/{2}_{3}.npy'.format(filedir, datadir, TYPE, AUG)
+    similar_verifit(loadpath)
 
 
 if __name__ == '__main__':
