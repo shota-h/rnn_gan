72ea98fa8bea8539242b4d657250274c4c0ededd
shota <shota-h>
2018-10-10 01:50:46 +0900

    change disp_subplot func

--- cca.py
+++ cca.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 from keras.models import model_from_json
 from keras import backend as K
 from keras.layers.merge import concatenate
+import csv
 SEED = 1
 np.random.seed(SEED)
 rn.seed(SEED)
@@ -31,30 +32,40 @@ datadir = args.datadir
 Class = args.Class
 l_ind = args.load
 
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.size'] = 24
 
 filedir = os.path.abspath(os.path.dirname(__file__))
-filepath = '{0}/{1}/{2}'.format(filedir, dirs, datadir)
+Loadpath = '{0}/{1}/{2}/class{3}'.format(filedir, dirs, datadir, Class, l_ind)
+filepath = '{0}/{1}/{2}/class{3}/load{4}'.format(filedir, dirs, datadir, Class, l_ind)
 config = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1, gpu_options=tf.GPUOptions(allow_growth=True, visible_device_list='1'), device_count={'GPU':0})
-
+if os.path.exists(filepath) is False:
+    os.makedirs(filepath)
 
 def trans_data(src, z):
     buff = np.copy(src)
     diff_src = [buff[:, i+1] - buff[:,i] for i in range(1, buff.shape[1]-1)]
     diff2_src = [buff[:, i+1] - 2*buff[:,i] + buff[:, i-1] for i in range(1, buff.shape[1]-1)]
-    lim = 10
-    diff_src = np.asarray(diff_src).T
-    uppeak_src = np.max(src[:, :lim], axis=1)
-    downpeak_src = np.min(src[:, lim:], axis=1)
-    uppeak_ind = np.argmax(src[:, :lim], axis=1)
-    downpeak_ind = np.argmin(src[:,lim:], axis=1)+ lim
+    if datadir == 'ECG200':
+        lim = 10
+        diff_src = np.asarray(diff_src).T
+        uppeak_src = np.max(src[:, :lim], axis=1)
+        downpeak_src = np.min(src[:, lim:], axis=1)
+        uppeak_ind = np.argmax(src[:, :lim], axis=1)
+        downpeak_ind = np.argmin(src[:,lim:], axis=1)+ lim
+    else:
+        uppeak_src = np.max(src, axis=1)
+        downpeak_src = np.min(src, axis=1)
+        uppeak_ind = np.argmax(src, axis=1)
+        downpeak_ind = np.argmin(src, axis=1)
     # plt.plot(downpeak_ind, 'blue')
     # plt.plot(uppeak_ind, 'red')
     # plt.savefig('{0}/test.png'.format(filepath))
     # plt.close()
-    if np.any(downpeak_ind < uppeak_ind):
-        del_ind = np.where(downpeak_ind < uppeak_ind)[0]
-        print('error')
-    peak_to_peak = downpeak_ind - uppeak_ind
+    # if np.any(downpeak_ind < uppeak_ind):
+    #     del_ind = np.where(downpeak_ind < uppeak_ind)[0]
+    #     print('error')
+    peak_to_peak = abs(downpeak_ind - uppeak_ind)
     mean_src = np.mean(src, axis=1)
     freq_lin = np.arange(0, src.shape[1], 1)
     freq_lin = freq_lin[:int(np.floor((src.shape[1]-1)/2))]
@@ -95,29 +106,49 @@ def disp_subplot(src, n=[1,1], name=None, axis_fix=False, ylim=[None, None],plot
     label = ['max', 'max ind', 'min', 'min ind', 'p2p', 'mean', 'meanfreq']
     if src.shape[1] != len(label):
         label = np.arange(0, src.shape[1], 1)
-    fig = plt.figure(figsize=(12,9))
-    fig.suptitle(name, fontsize=20)
+    # fig = plt.figure(figsize=(12,9))
+    fig = plt.figure(figsize=(12*nx,3*ny))
+    # fig.suptitle(name, fontsize=20)
     if ymax is None and ymin is None:
         ymax = src.max()
         ymin = src.min()
         ylim = [np.min(src), np.max(src)]
-
+    e = ['k' for i in range(7)]
+    bar_width = 0.5
     for i, j in enumerate(src):
         ax = fig.add_subplot(ny, nx, i+1)
         # ax.scatter(label, j)
         if plot_type == 'line':
-            ax.plot(j, linestyle='-', marker='.')
+            ax.plot(j, linestyle='-', marker='.', color='k')
+            if i+1 == ny*nx:
+                ax.set_xticklabels(np.arange(len(j)))
+            else:
+                ax.set_xticklabels([])
         elif plot_type == 'bar':
-            ax.bar(range(len(j)), j, tick_label=label)
+            # ax.bar(range(len(j)), j, tick_label=label, color='w', edgecolor='k')
+            ax.bar(np.arange(len(j)), j, bar_width, color='w', edgecolor=e)
+            ax.set_xticks(np.arange(len(j)))
+            # if src[-1] == j:
+            if i+1 == ny*nx:
+                ax.set_xticklabels(label)
+            else:
+                ax.set_xticklabels([])
+                # plt.tick_params(color='w')
             # ax.tick_params(labelsize=20)
             ax.hlines([0], 0, len(j)-1, 'black', linestyles='dashed')
-            for x, y in zip(range(len(j)), j):
-                plt.text(x, y, '{:.2f}'.format(y), ha='center', va='bottom')
-
+            # for x, y in zip(range(len(j)), j):
+                # plt.text(x, y*1.1, '{:.2f}'.format(y), ha='center', va='top')
         if axis_fix:
             ax.set_ylim(ymin, ymax)
-    fig.savefig('{0}/{1}.png'.format(filepath, name))
+    plt.tight_layout()
+    # plt.tick_params(labelsize=30)
+    fig.savefig('{0}/{1}.eps'.format(filepath, name))
     plt.close()
+    if plot_type == 'bar':
+        with open('{0}/{1}.csv'.format(filepath, name), 'w') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            for i in src:
+                writer.writerow(i)
 
 
 def center_scale(src):
@@ -138,6 +169,7 @@ def chi_test(corr, n_sample, p, q):
 
 def calc_cca(x1, x2, low_corr=0):
     label = ['max', 'min', 'p2p', 'mean', 'meanfreq']
+    # label = ['max', 'max ind', 'min', 'min ind', 'p2p', 'mean', 'meanfreq']
     n_comp = args.n_comp
     if n_comp > np.minimum(x1.shape[1], x2.shape[1]):
         n_comp =  np.minimum(x1.shape[1], x2.shape[1])
@@ -154,6 +186,9 @@ def calc_cca(x1, x2, low_corr=0):
         # print("{0}:{1:.4f}".format(i, corr))
         print("{0}:{1}".format(i, corr))
     assert len(ind) > 0, print(ind)
+    with open('{0}/corr_class{1}.csv'.format(Loadpath, Class), 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(corr_list)
 
     if n_comp == x1.shape[1]:
         chi_test(corr_list, x1.shape[0], x1.shape[1], x2.shape[1])
@@ -180,7 +215,7 @@ def calc_cca(x1, x2, low_corr=0):
 
 
 def control_z(src, c):
-    loadpath = '{0}/lstm-gan/{1}_split_No0'.format(filedir, datadir)
+    loadpath = '{0}/lstm-gan/backup1007/{1}_split_No0'.format(filedir, datadir)
     global seq_length, feature_count
     seq_length = src.shape[1]
     feature_count = 1
@@ -204,53 +239,50 @@ def control_z(src, c):
 def main():
     x = np.load('{0}/dataset/{1}/train0.npy'.format(filedir, datadir))
     buff = []
+    label = ['max', 'max ind', 'min', 'min ind', 'p2p', 'mean', 'meanfreq']
     np.set_printoptions(formatter={'float': '{: 0.4f}'.format})
     for cc, i in enumerate(np.unique(x[..., -1])):
         src = x[x[..., -1]==i, :-1]
         trans_x1, trans_x2 = trans_data(src, src)
-        print(trans_x1.mean(axis=0))
-        print(trans_x1.std(axis=0))
         plt.figure(figsize = (16, 9))
         plt.plot(src.mean(axis=0))
-        plt.plot(src[:3].T)
+        # plt.plot(src[:3].T)
         plt.ylim([0,1])
         plt.title('{}'.format(cc))
-        plt.savefig('{0}/CCA/ECG200/train_average_class{1}.png'.format(filedir, cc))
+        plt.savefig('{0}/CCA/{2}/train_average_class{1}.png'.format(filedir, cc, datadir))
         plt.close()
         plt.figure(figsize = (16, 9))
         plt.bar(range(len(trans_x1.mean(axis=0))), trans_x1.mean(axis=0), yerr = trans_x1.std(axis=0))
         plt.title('{}'.format(cc))
-        plt.savefig('{0}/CCA/ECG200/train_feature_hist_class{1}.png'.format(filedir, cc))
+        # plt.xticklabels(label)
+        plt.savefig('{0}/CCA/{2}/train_feature_hist_class{1}.png'.format(filedir, cc, datadir))
         plt.close()
         src = src.mean(axis=0)
         buff.append(src)
     buff = np.asarray(buff)
     trans_x1, trans_x2 = trans_data(buff, buff)
-    print(trans_x1)
-    return
 
-    x = np.load('{0}/class{1}.npy'.format(filepath, Class))
+    x = np.load('{0}/class{1}.npy'.format(Loadpath, Class))
     x1 = x[..., 0]
     x2 = x[..., 1]
     trans_x1, trans_x2 = trans_data(x1, x2)
     x_loadings, y_loadings = calc_cca(trans_x1, trans_x2, low_corr=0.45)
-    # y_loadings = (y_loadings - y_loadings.min(axis=0)) /(y_loadings.max(axis=0) - y_loadings.min(axis=0))
-    assert x_loadings.shape[1] > l_ind, 'print load index{0}'.format(l_ind) 
+    assert x_loadings.shape[1] > l_ind, 'print load index{0}'.format(l_ind)
     p = l_ind
     z = [y_loadings[:, p] * i for i in np.arange(0.5, 1.5, 1/100)]
     z = np.asarray(z)[..., None]
     g_z = control_z(src=z, c=Class)
 
     trans_x, trans_y = trans_data(g_z[..., 0], g_z[..., 1])
-    ind = range(0, 100, 15)
-    disp_subplot(g_z[ind, :, 0], n=[3,2], plot_type='line', name='control_gz_based_on_loading{1}_class{0}'.format(Class, p), axis_fix=True, ylim=[0,1])
-    disp_subplot(trans_x[ind], n=[3,2], plot_type='bar', name='gz_based_on_loading{1}_class{0}'.format(Class, p), axis_fix=True)
-    disp_subplot(trans_y[ind], n=[3,2], plot_type='line', name='z_based_on_loadings{1}_class{0}'.format(Class, p), axis_fix=True)
+    ind = range(0, 100, 30)
+    disp_subplot(g_z[ind, :, 0], n=[3,1], plot_type='line', name='control_gz_based_on_loading{1}_class{0}'.format(Class, p), axis_fix=True, ylim=[0,1])
+    disp_subplot(trans_x[ind], n=[3,1], plot_type='bar', name='gz_based_on_loading{1}_class{0}'.format(Class, p), axis_fix=True)
+    disp_subplot(trans_y[ind], n=[3,1], plot_type='line', name='z_based_on_loadings{1}_class{0}'.format(Class, p), axis_fix=True)
     return
 
     # morphing_disp()
     # x = np.load('{0}/walk_pos{2}_class{1}.npy'.format(filepath, Class, args.pos))
-    x = np.load('{0}/walk2_class{1}.npy'.format(filepath, Class, args.pos))
+    x = np.load('{0}/walk2_class{1}.npy'.format(Loadpath, Class, args.pos))
     x1 = x[..., 0]
     x2 = x[..., 1]
     trans_x1, trans_x2 = trans_data(x1, x2)
