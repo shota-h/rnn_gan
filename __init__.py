c8f3981a40539f840a7b4544a0035a1473d70efe
shota <shota-h>
2018-11-09 21:27:01 +0900

    add function of mmd and dtw

--- __init__.py
+++ __init__.py
@@ -3,8 +3,86 @@ import datetime
 import json
 import requests
 import csv
+import itertools
+import os
+import sys
 
 
+def test_dtws(vec1, vec2, band_flag=False, num_band=0):
+    dst = []
+    for k1, k2 in itertools.product(range(vec1.shape[0]),range(vec2.shape[0])):
+        dtw_dis = test_dtw(vec1[k1], vec2[k2], band_flag=band_flag, num_band=num_band)
+        dst.append(dtw_dis)
+    dst = np.array(dst).reshape(vec1.shape[0], vec2.shape[0])
+    mean_dst = np.mean(dst, axis=1)
+    std_dst = np.std(dst, axis=1)
+
+    return dst, mean_dst, std_dst
+
+def backprop(cost_mat):
+    iy, ix = cost_mat.shape
+    iy -= 1
+    ix -= 1
+    warp_path = []
+    while ix != 0 or iy != 0:
+        ind = [[iy-1, ix], [iy, ix-1], [iy-1, ix-1]]
+        ref = np.argmin([cost_mat[iiy, iix] for iiy, iix in ind])
+        iy, ix = ind[ref]
+        warp_path.append(ind[ref])
+    return warp_path
+
+
+def dtw(vec1, vec2, band_flag=False, num_band=0):
+    d = np.zeros([len(vec1)+1, len(vec2)+1])
+    d[:] = np.inf
+    d[0, :] = np.inf
+    d[0, 0] = 0
+    if band_flag is False: num_band = d.shape[0]
+    for i, j in itertools.product(range(1, d.shape[0]), range(1, d.shape[1])):
+        if (num_band - 1) < abs(i - j): continue
+        cost = np.power(vec1[i-1]-vec2[j-1], 2)
+        d[i, j] = cost + min(d[i-1, j], d[i, j-1], d[i-1, j-1])
+    warp_path = backprop(d)
+
+    return d[-1,-1], warp_path
+
+
+def dtws(src1, src2, band_flag=False, num_band=0):
+    dst = []
+    for k1, k2 in itertools.product(range(src1.shape[0]),range(src2.shape[0])):
+        dtw_dst = dtw(src1[k1], src2[k2])
+        dst.append(dtw_dist)
+    dst = np.array(dst).reshape(vec1.shape[0], vec2.shape[0])
+    mean_dst = np.mean(dst, axis=1)
+    std_dst = np.std(dst, axis=1)
+    return dst, mean_dst, std_dst
+
+
+def test_dtw(vec1, vec2, band_flag=False, num_band=0):
+    d = np.zeros([len(vec1)+1, len(vec2)+1])
+    d[:] = np.inf
+    d[0, :] = np.inf
+    d[0, 0] = 0
+    if band_flag is False: num_band = d.shape[0]
+    for i, j in itertools.product(range(1, d.shape[0]), range(1, d.shape[1])):
+        if (num_band - 1) < abs(i - j): continue
+        cost = np.sqrt(np.power(vec1[i-1]-vec2[j-1], 2))
+        d[i, j] = cost + min(d[i-1, j], d[i, j-1], d[i-1, j-1])
+    
+    return d[-1, -1]
+
+
+def mse(vec1, vec2):
+    dst = []
+    for k1, k2 in itertools.product(range(vec1.shape[0]), range(vec2.shape[0])):
+        diff = np.linalg.norm(vec1[k1] - vec2[k2])
+        dst.append(diff)
+
+    dst = np.array(dst).reshape(vec1.shape[0], vec2.shape[0])
+    mean_dst = np.mean(dst, axis=1)
+    std_dst = np.std(dst, axis=1)
+
+    return dst, mean_dst, std_dst
 
 
 def re_label(src, c_label):
@@ -80,25 +158,34 @@ def gauss_dist_plot(x1, x2, path, name, num_f, num=10000):
     plt.savefig('{0}/figure/{1}.png'.format(path, name))
     plt.close()
 
+
+def dtw_kernel(src1, src2, sigma=1.0):
+    dtw_dist, warp_path = dtw(src1, src2)
+    src_norm = dtw_dist**2
+    return np.exp(-src_norm/sigma**2) / len(warp_path)
+
+
 def gauss_kernel(src1, src2, sigma=1.0):
     src_norm = np.linalg.norm(src1 - src2)**2
-    return k = np.exp(-src_norm/sigma**2)
+    return np.exp(-src_norm/sigma**2)
 
 
-def expected_gauss_kernel(src1, src2):
+def expected_kernel(src1, src2, kernel='gauss'):
     sum_k = 0
     num_src = min(src1.shape[0], src2.shape[0])
     for i, j in itertools.combinations(range(num_src), 2):
-        gauss_kernel(src1[i], src2[j])
-        sum_k += k
-    return sum_k / len(list(itertools.combinations(num_src), 2))
+        if kernel == 'gauss':
+            sum_k += gauss_kernel(src1[i], src2[j])
+        elif kernel == 'dtw':
+            sum_k += dtw_kernel(src1[i], src2[j])
+    return sum_k / len(list(itertools.combinations(range(num_src), 2)))
 
 
-def mmd(src1, src2):
+def mmd(src1, src2, kernel='gauss'):
     assert src1.ndim == src2.ndim, print('src1 and src2 is same dimension')
     assert src1.shape[-1] == src2.shape[-1], print('src1 and src2 is same features')
-    
-    k1 = gauss_kernel(src1, src1)
-    k2 = gauss_kernel(src1, src2)
-    k3 = gauss_kernel(src2, src2)
+
+    k1 = expected_kernel(src1, src1, kernel=kernel)
+    k2 = expected_kernel(src1, src2, kernel=kernel)
+    k3 = expected_kernel(src2, src2, kernel=kernel)
     return k1 - 2*k2 + k3
